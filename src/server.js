766a58e7f636d237ac454b8d0ecf99336ee68a2e
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-06-08 16:44:57 +0700

    Edit link Vercel

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const corsOptions = {
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
-    'https://vercel.com/nantanat-poyomratanasins-projects/group-project-03-sprint-02/EPSZHEJh8b2aTSUXechXtBQeqEUa'
+    'https://group-project-03-sprint-02-seven.vercel.app'
   ], // frontend domain
   credentials: true // ✅ allow cookies to be sent
 };
